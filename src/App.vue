3647783ea277dde52aae0e0dfd2d497e0a38b15e
kyounghwan kim <dolkys123@sju.ac.kr>
2022-08-25 18:32:26 +0900

    vuetify setting

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar></v-app-bar>
+    <v-navigation-drawer width="10%">
+      <v-row>
+        <v-col>
+          <v-icon>mdi-domain</v-icon>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col align-self>
+          <v-icon>mdi-domain</v-icon>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col align-self>
+          <v-icon>mdi-domain</v-icon>
+        </v-col>
+      </v-row>
+    </v-navigation-drawer>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    routelist: [
+      {
+        name: "웹사이트 관리",
+        link: "/website",
+      },
+      {
+        name: "계정 관리",
+        link: "/management",
+      },
+    ],
+    selectedItem: 1,
+    items: [
+      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Audience", icon: "mdi-account" },
+      { text: "Conversions", icon: "mdi-flag" },
+    ],
+  }),
+};
+</script>
